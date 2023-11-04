d346d25eb1ff0ba13478dd82af8674a9fe41ac07
Saúl Camacho <electrohead1200@gmail.com>
2023-11-04 13:09:14 -0600

    Funcionalidad Agregar Tarea y Listar Tareas

--- app.js
+++ app.js
@@ -15,16 +15,46 @@ function displayMenu() {
     console.log("2. Listar Tareas");
     console.log("3. Completar Tareas");
     console.log("4. Salir");
+    console.log("\n");
+}
+
+function addTask() {
+    rl.question(chalk.bgMagentaBright("Escribe la tarea: "), (task) => {
+        tasks.push({ task, completed: false });
+        console.log(chalk.green.bold("Tarea agregada con éxito"));
+        displayMenu();
+        chooseOption();
+    });
+}
+
+function listTasks() {
+    console.log(chalk.yellow.bold("\n Lista de Tareas \n"));
+    if (tasks.length === 0) {
+        console.log(chalk.green.bold("No hay Tareas por hacer\n\n"));
+    } else {
+        tasks.forEach((task, index) => {
+            let status = task.completed ? "OK" : "NO";
+
+            if (status) {
+                console.log(chalk.green.bold(`${index + 1}. ${status} - ${task.task}`));
+            } else {
+                console.log(chalk.redBright(`${index + 1}. ${status} - ${task.task}`));
+            }
+        });
+    }
+
+    displayMenu();
+    chooseOption();
 }
 
 function chooseOption() {
-    rl.question("Digita el número de tu opción:", (choice) => {
+    rl.question("Digita el número de tu opción: ", (choice) => {
         switch (choice) {
             case "1":
-                console.log("Crear Tarea")
+                addTask();
                 break;
             case "2":
-                console.log("Listar Tareas")
+                listTasks();
                 break;
             case "3":
                 console.log("Completar Tarea")
